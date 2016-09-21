76291501710d6075a2d160e4e042d6e468127c34
fdub <fabian.dubberke@posteo.de>
2016-09-21 11:10:41 +0200

    Added frame size to file name to queue processing

--- src/enframe.js
+++ src/enframe.js
@@ -10,84 +10,80 @@ require.config({
 define('enframe', ['require', 'Rx', 'jquery'], function(require) {
     Rx = require('Rx');
     $ = require('jquery');
+    var emptyCallback = function() { };
     var me = {};
     var frames = [];
-    var isInitalized = false;
-    var initFailed = false;
 
-    String.prototype.format = String.prototype.f = function() {
-        var s = this,
-            i = arguments.length;
+    me.init = function(framesSelector) {
+        var initFrame;
+        var subscribeInit;
+        var finishOrRetryInit;
 
-        while (i--) {
-            s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
-        }
-        return s;
-    };
-
-    me.init = function(framesSelector, callback, options) {
-        var i;
-        var framesLoading = 0;
-
-        function setInitialFrameSizeIfAvailable(frame) {
-            framesLoading--;
-            
-            if (frame.img.height === 0 || frame.img.width === 0) {
-                frame.retries--;
-                if (frame.retries > 0) {
-                    subscribeInitialFrameSizeCheck(frame);
-                } else {
-                    throw 'Image could not be loaded.';
-                }
-            } else {
-                frame.outer = { 
-                    width: frame.img.width,
-                    height: frame.img.height 
-                };
-                frame.inner = {
-                    width: frame.outer.width - frame.padding.left - frame.padding.right,
-                    height: frame.outer.height - frame.padding.top - frame.padding.bottom
-                };
-                frame.inner.ratio = frame.inner.width / frame.inner.height;
-
-                frame.initSubscription.dispose();
-                frames.push(frame);   
-                if (framesLoading === 0) {
-                    callback();
-                }
-            }
-        }
-
-        function subscribeInitialFrameSizeCheck(frame) {
-            framesLoading++;
-
-            frame.initSubscription.setDisposable(
-                Rx.Observable
-                    .of(frame)
-                    .delay(20 * (6 - frame.retries) * (6 - frame.retries))
-                    .subscribe(setInitialFrameSizeIfAvailable));
-        }
-
-        function initFrame(frame, frameUrl) {
+        initFrame = function(frame, frameUrl) {
             var frameUrlSplit = frameUrl.split('_');
             
             frame.img.src = frameUrl;
             frame.retries = 5;
-            frame.padding = {
-                top: parseInt(frameUrlSplit[1]),
-                right: parseInt(frameUrlSplit[2]),
-                bottom: parseInt(frameUrlSplit[3]),
-                left: parseInt(frameUrlSplit[4])                
+            
+            frame.outer = {
+                width: parseInt(frameUrlSplit[1]),
+                height: parseInt(frameUrlSplit[2]),
             };
-            frame.initSubscription = new Rx.SerialDisposable();
+            
+            frame.padding = {
+                top: parseInt(frameUrlSplit[3]),
+                right: parseInt(frameUrlSplit[4]),
+                bottom: parseInt(frameUrlSplit[5]),
+                left: parseInt(frameUrlSplit[6])                
+            };
 
-            subscribeInitialFrameSizeCheck(frame);
-        }
+            frame.inner = {
+                width: frame.outer.width - frame.padding.left - frame.padding.right,
+                height: frame.outer.height - frame.padding.top - frame.padding.bottom
+            };
+            frame.inner.ratio = frame.inner.width / frame.inner.height;
+
+            frame.isInitialized = false;
+            frame.initSubscription = new Rx.SerialDisposable();
+            frame.onInitalized = [];
+
+            subscribeInit(frame);
+            return frame;
+        };
+
+        subscribeInit = function(frame) {
+            frame.initSubscription.setDisposable(
+                Rx.Observable
+                    .of(frame)
+                    .delay(20 * (6 - frame.retries) * (6 - frame.retries))
+                    .subscribe(finishOrRetryInit));
+        };
+
+        finishOrRetryInit = function(frame) {
+            if (frame.img.height === 0 || frame.img.width === 0) {
+                frame.retries--;
+                if (frame.retries > 0) {
+                    subscribeInit(frame);
+                } else {
+                    throw 'Image could not be loaded.';
+                }
+            } else {
+                frame.initSubscription.dispose();
+                frame.isInitialized = true;
+
+                $(frame.onInitalized).each(function(_, fn) {
+                    fn();
+                });
+                frame.onInitalized = [];
+            }
+        };
 
         $(framesSelector).each(function(_, el) {
             var frameUrl = el.src;
 
-            initFrame({ img: new Image() }, frameUrl);
+            var frame = initFrame({ img: new Image() }, frameUrl);
+            
+            frames.push(frame);   
         });
     };
 
@@ -110,6 +106,7 @@ define('enframe', ['require', 'Rx', 'jquery'], function(require) {
             .css('top', top + '%')
             .css('width', 100 - right - left + '%')
             .css('height', 100 - top - bottom + '%');
+
         $container.find('.nf-shadow')
             .css('left', left + '%')
             .css('top', top + '%')
@@ -124,39 +121,56 @@ define('enframe', ['require', 'Rx', 'jquery'], function(require) {
             })[0];
     }
 
-    me.wrap = function(selector) {
-        $(selector).each(function(_, img) {
-            var src = $(img)[0].src || $(img).find('img')[0].src;
-            var frame = selectFrame(img.clientWidth / img.clientHeight);
-            
-            var imgFactor = img.clientWidth / frame.inner.width; 
+    me.wrapOne = function(img, callback) {
+        var src = $(img)[0].src || $(img).find('img')[0].src;
+        var frame = selectFrame(img.clientWidth / img.clientHeight);
+        
+        var width = img.clientWidth;
+        var innerWidth = width * frame.inner.width / frame.outer.width;
+        var innerHeight = innerWidth * img.clientHeight / img.clientWidth;
+        var height = innerHeight * frame.outer.height / frame.inner.height;
+        
+        var $container = $(img)
+            .wrap('<div></div>')
+            .parent()
+            .append('<div class="nf-image"></div>')
+            .append('<div class="nf-image"></div>')
+            .append('<div class="nf-shadow"></div>')
+            .addClass('nf-container');
 
-            var width = img.clientWidth;
-            var innerWidth = width * frame.inner.width / frame.outer.width;
-            var innerHeight = innerWidth * img.clientHeight / img.clientWidth;
-            var height = innerHeight * frame.outer.height / frame.inner.height;
-            
-            var $container = $(img)
-                .wrap('<div></div>')
-                .parent()
-                .append('<div class="nf-image"></div>')
-                .append('<div class="nf-image"></div>')
-                .append('<div class="nf-shadow"></div>')
-                .addClass('nf-container');
+        $container.find('.nf-image').first().css('background-image', 'url(' + src + ')');
+        $container.find('.nf-image').last().css('background-image', 'url(' + frame.img.src + ')');
 
-            $container.find('.nf-image').first().css('background-image', 'url(' + src + ')');
-            $container.find('.nf-image').last().css('background-image', 'url(' + frame.img.src + ')');
+        setContainerSize($container[0], frame, width, height);
+        
+        if ($container.find(img).is('img')) {
+            $container.find(img).remove();
+        } else {
+            $container.find(img).find('img').remove();
+            $container.find(img).detach().appendTo($container.find('.nf-shadow'));
+        }
 
-            setContainerSize($container[0], frame, width, height);
-            
-            if ($container.find(img).is('img')) {
-                $container.find(img).remove();
-            } else {
-                $container.find(img).find('img').remove();
-                $container.find(img).detach().appendTo($container.find('.nf-shadow'));
-            }
-
+        var show = function() {
             $container.animate({opacity: '1'}, 100);
+            (callback || emptyCallback)();
+        };
+        if (frame.isInitialized) {
+            show();
+        } else {
+            frame.onInitalized.push(show);
+        }
+    };
+
+    me.wrap = function(selector, callback) {
+        var imgCounter = 0; 
+        $(selector).each(function(_, img) {
+            imgCounter++;
+            me.wrapOne(img, function() {
+                imgCounter--;
+                if (imgCounter === 0) {
+                    (callback || emptyCallback)();
+                }
+            });
         });
     };    
     return me;
